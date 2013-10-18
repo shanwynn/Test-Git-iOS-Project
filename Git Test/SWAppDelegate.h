5af43219005d9be2401ccb18caef8f33dc400024
Shannon Wynn <shannonwynn@mac.com>
2013-10-17 20:02:39 -0400

    Initial Commit

---
+++ Git Test/SWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWAppDelegate.h
+//  Git Test
+//
+//  Created by Shannon Wynn on 10/17/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/SWViewController.h
@@ -0,0 +1,13 @@
+//
+//  SWViewController.h
+//  Git Test
+//
+//  Created by Shannon Wynn on 10/17/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWViewController : UIViewController
+
+@end
