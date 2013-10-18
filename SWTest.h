980e69a0cae390ecd527597c1132a16dfb76e94a
Shannon Wynn <shannonwynn@mac.com>
2013-10-17 20:17:01 -0400

    added a test class

---
+++ SWTest.h
@@ -0,0 +1,13 @@
+//
+//  SWTest.h
+//  Git Test
+//
+//  Created by Shannon Wynn on 10/17/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWTest : NSObject
+
+@end
---
+++ SWTest.m
@@ -0,0 +1,13 @@
+//
+//  SWTest.m
+//  Git Test
+//
+//  Created by Shannon Wynn on 10/17/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import "SWTest.h"
+
+@implementation SWTest
+
+@end
